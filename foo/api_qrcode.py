9878d03dd03b712f35341c6cdf5cf544e83db76f
tomasino.zh <tomasino.zhang@qq.com>
2018-10-15 18:48:06 +0800

    qrcode default size=5

--- foo/api_qrcode.py
+++ foo/api_qrcode.py
@@ -37,8 +37,9 @@ from qrcode import *
 
 @swagger.model()
 class QrcodeReq:
-    def __init__(self, url):
+    def __init__(self, url, size=5):
         self.url = url
+        self.size = size
 
 
 # /qrcode/api/qrcode
@@ -60,13 +61,25 @@ class ApiQrcodeXHR(tornado.web.RequestHandler):
         logging.info("POST %r", self.request.uri)
         logging.debug("body %r", self.request.body)
 
-        data = json_decode(self.request.body)
-        _url = data['url']
+        _url = None
+        _size = 5
+        try:
+            data = json_decode(self.request.body)
+            _url = data['url']
+            if data.has_key("size"):
+                _size = data['size']
+        except:
+            logging.warn("Bad Request[400]: generate qrcode req_body=[%r]", self.request.body)
+
+            self.set_status(200) # Bad Request
+            self.write(JSON.dumps({"errCode":400,"errMsg":"Bad Request"}))
+            self.finish()
+            return
 
         qr = QRCode(
             # version值为1~40的整数,控制二维码的大小,(最小值是1,是个12*12的矩阵)
             # 如果想让程序自动确定,将值设置为 None 并使用 fit 参数即可
-            version=5,
+            version=_size,
             # error_correction: 控制二维码的错误纠正功能,可取值下列4个常量
             #   ERROR_CORRECT_L: 大约7%或更少的错误能被纠正
             #   ERROR_CORRECT_M(默认): 大约15%或更少的错误能被纠正
@@ -74,7 +87,7 @@ class ApiQrcodeXHR(tornado.web.RequestHandler):
             #   ERROR_CORRECT_H: 大约30%或更少的错误能被纠正
             error_correction=ERROR_CORRECT_L,
             # 控制二维码中每个小格子包含的像素数
-            box_size=2,
+            box_size=10,
             # 控制边框(二维码与图片边界的距离)包含的格子数(默认为4,是相关标准规定的最小值)
             border=4,
         )
@@ -96,7 +109,7 @@ class ApiQrcodeXHR(tornado.web.RequestHandler):
 
         img_url = self.request.protocol + "://" + self.request.host
         img_url = img_url + '/static/qrcode/' + _datehour + "/" + _id + '.png'
-        logging.info("generate qrcode from=[%r] img_url=[%r]", _url, img_url)
+        logging.info("Success[200]: generate qrcode from=[%r] img_url=[%r]", _url, img_url)
         self.set_status(200) # Success
         self.write(JSON.dumps({"errCode":200,"errMsg":"Success","rs":img_url}))
         self.finish()
